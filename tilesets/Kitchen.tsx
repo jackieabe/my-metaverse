d0b7700554682fd4103b1f9e8b0242bbfdcda615
jackieabe <jptwabe@gmail.com>
2026-01-18 20:48:19 +0800

    Add files via upload

---
+++ tilesets/Kitchen.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Kitchen" tilewidth="32" tileheight="32" tilecount="72" columns="4">
+ <image source="map-starter-kit-master/tilesets/Kitchen.png" width="144" height="592"/>
+</tileset>
